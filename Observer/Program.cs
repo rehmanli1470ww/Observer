a2d1345778559416a5344e87a6be47300a0500f9
rehmanli1470ww <144922823+rehmanli1470ww@users.noreply.github.com>
2023-12-09 21:35:37 +0400

    Add project files.

---
+++ Observer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IObserver
+{
+    void Update(string message);
+}
+
+public class WeatherStation
+{
+    private List<IObserver> observers = new List<IObserver>();
+    private int temperature;
+
+    public int Temperature
+    {
+        get { return temperature; }
+        set
+        {
+            temperature = value;
+            NotifyObservers();
+        }
+    }
+
+    public void Attach(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void Detach(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    private void NotifyObservers()
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update($"Temperature has been updated: {Temperature}°C");
+        }
+    }
+}
+
+public class Display : IObserver
+{
+    public void Update(string message)
+    {
+        Console.WriteLine("Display: " + message);
+    }
+}
+
+public class Logger : IObserver
+{
+    public void Update(string message)
+    {
+        Console.WriteLine("Logger: " + message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        WeatherStation weatherStation = new WeatherStation();
+
+        Display display = new Display();
+        Logger logger = new Logger();
+
+        weatherStation.Attach(display);
+        weatherStation.Attach(logger);
+
+        weatherStation.Temperature = 25;
+
+        weatherStation.Detach(logger);
+        weatherStation.Temperature = 30;
+    }
+}
